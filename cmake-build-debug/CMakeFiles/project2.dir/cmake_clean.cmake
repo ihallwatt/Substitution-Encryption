88c4ed8f2816bee0b439bf3c33c7181422085e0d
Alistair The Gecko <ihallwatt@gmail.com>
2019-03-03 16:17:22 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/project2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project2.dir/p2.c.obj"
+  "CMakeFiles/project2.dir/functions.c.obj"
+  "project2.pdb"
+  "project2.exe"
+  "project2.exe.manifest"
+  "libproject2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/project2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
